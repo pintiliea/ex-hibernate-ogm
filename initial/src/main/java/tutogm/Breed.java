55c2e749941f77d79b05e9f0ccc4a17ca1f3fce5
Alex Pintilie <pintiliea81@gmail.com>
2016-11-14 15:55:22 +0100

    initial

---
+++ initial/src/main/java/tutogm/Breed.java
@@ -0,0 +1,27 @@
+
+@Entity
+public class Breed {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	private String id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+}
