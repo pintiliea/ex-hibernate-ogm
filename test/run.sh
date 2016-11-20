1d55fde3a1f4401b664712090282a703b769b288
Alex Pintilie <pintiliea81@gmail.com>
2016-11-20 23:05:14 +0100

    Let Travis build (maven only currently)

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)
 
-cd ../complete
-
 mvn clean package
 ret=$?
 if [ $ret -ne 0 ]; then
@@ -10,14 +8,12 @@ exit $ret
 fi
 rm -rf target
 
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
-
-cd ../initial
+# ./gradlew build
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf build
 
 mvn clean compile
 ret=$?
@@ -26,11 +22,11 @@ exit $ret
 fi
 rm -rf target
 
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
+# ./gradlew compileJava
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf build
 
 exit
